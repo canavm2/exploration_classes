ac9c750b8ec8fec17f685a409e5a537aea489762
canavm2 <canavm2@gmail.com>
2022-01-12 08:30:56 -0700

    minor updates, switchign computer

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using exploration_classes;
 Console.WriteLine("Testing");
 
+string path = "names.csv";
+List<string> female_names = new List<string>();
+List<string> male_names = new List<string>();
+List<string> nb_names = new List<string>();
+List<string> last_names = new List<string>();
+
+string[] lines = System.IO.File.ReadAllLines(path);
+Console.WriteLine(lines);
+
 CompanyMember citizen = new exploration_classes.CompanyMember();
 citizen.describeCitizen();
-List<string> female_names = new List<string>()
-{
-    "ann","catie"
-};
+
 
 PlayerCompany playerCompany = new PlayerCompany("Mashers", citizen);
