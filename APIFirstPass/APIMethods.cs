b01bcdc7689b2e74976f96ea7c123816b95c5978
canavm2 <canavm2@gmail.com>
2022-01-25 16:06:13 -0700

    added API to repository and merged with exploration_classes

---
+++ APIFirstPass/APIMethods.cs
@@ -0,0 +1,27 @@
+﻿namespace APIMethods;
+using People;
+using Company;
+
+public class CitizenDB
+{
+    public static string ReturnCitizen(CitizenCache cache)
+    {
+        Citizen citizen = cache.GetRandomCitizen();
+        return citizen.Describe();
+    }
+    public static string ReturnCitizenFromCompany(PlayerCompany playercompany, int id)
+    {
+        if (id == 0) return playercompany.Advisors["master"].Describe();
+        else if (id > 0 && id < 6)
+        {
+            string advisorkey = $"advisor{id}";
+            return playercompany.Advisors[advisorkey].Describe();
+        }
+        else if (id >= 6 && id < playercompany.Advisors.Count)
+        {
+            string advisorkey = $"bench{id-6}";
+            return playercompany.Advisors[advisorkey].Describe();
+        }
+        else return $"Error: id must be between 0 and {playercompany.Advisors.Count}";
+    }
+}
---
+++ APIFirstPass/Program.cs
@@ -0,0 +1,59 @@
+using APIMethods;
+using FileTools;
+using Relation;
+using People;
+using Company;
+
+#region shitidontunderstand
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+#endregion
+
+
+FileTool fileTool = new FileTool();
+IndexId index = new IndexId(fileTool.ReadIndex());
+RelationshipCache relationshipcache = fileTool.ReadRelationshipCache("relationships");
+CitizenCache citizens = fileTool.ReadCitizens("citizens");
+PlayerCompany playercompany = fileTool.ReadCompany("company");
+
+
+
+#region HTTPCallMapping
+app.MapGet("/test", () => CitizenDB.ReturnCitizen(citizens));
+app.MapGet("/company", () => playercompany.Describe());//() => CitizenDB.ReturnCitizen(citizens));
+app.MapGet("/company/citizen/{id}", (int id) => CitizenDB.ReturnCitizenFromCompany(playercompany, id));//() => CitizenDB.ReturnCitizen(citizens));
+#endregion
+
+
+
+app.Run();
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//       new WeatherForecast
+//       (
+//           DateTime.Now.AddDays(index),
+//           Random.Shared.Next(-20, 55),
+//           summaries[Random.Shared.Next(summaries.Length)]
+//       ))
+//        .ToArray();
+//    return forecast;
+//})
+//    .WithName("GetWeatherForecast");
